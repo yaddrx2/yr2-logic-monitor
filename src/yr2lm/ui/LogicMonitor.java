fb957f3bdbbed0d16e448de9eae9e56c159b8fdd
yaddrx2 <yaddrx2@foxmail.com>
2024-03-15 13:24:12 +0800

    add edit Table

--- src/yr2lm/ui/LogicMonitor.java
+++ src/yr2lm/ui/LogicMonitor.java
@@ -27,22 +27,101 @@ public class LogicMonitor extends Monitor {
     private boolean showVarPage = true, showEditPage = false;
     private final Table varTools, varPage, editTools, editPage;
 
-    private class VarTable extends Table {
+    private class VarCell extends Table {
         public LExecutor.Var var;
 
-        public VarTable(LExecutor.Var var, String varName) {
+        public VarCell(LExecutor.Var var, String varName) {
             super();
             this.var = var;
             table(t -> {
                 t.table(tt -> tt.labelWrap(varName).grow()).grow().pad(0, 10, 0, 5);
                 t.table(tt -> tt.labelWrap(() -> formatVarText(this.var)).grow()).grow().pad(0, 5, 0, 10);
             }).minHeight(35).growX();
+            varCells.add(this);
+        }
+
+        private String formatVarText(LExecutor.Var var) {
+            if (var.isobj)
+                if (var.objval instanceof String) return '"' + var.objval.toString() + '"';
+                else if (var.objval == null) return "null";
+                else if (var.objval instanceof Unit unit) {
+                    return '[' + unit.type.name + '#' + unit.id + "]\n[" + BigDecimal.valueOf(unit.flag).stripTrailingZeros().toPlainString() + "]";
+                } else if (var.objval instanceof Building building) {
+                    return building.block.name + '#' + building.id;
+                } else return var.objval.toString();
+            else return BigDecimal.valueOf(var.numval).stripTrailingZeros().toPlainString();
+        }
+    }
+
+    private class CodeCell extends Table {
+        private final String line;
+        public String code;
+        private final String codeOrigin;
+
+        private boolean edit;
+
+        public CodeCell(String line, String code, boolean edit) {
+            super();
+            this.line = line;
+            this.code = code;
+            this.codeOrigin = code;
+            this.edit = edit;
+            codeCellBuild();
+            if (!edit) codeCells.add(this);
+        }
+
+        private void codeCellBuild() {
+            clear();
+            table(t -> {
+                t.labelWrap(line).width(60).pad(0, 10, 0, 5);
+                if (edit) t.field(code, s -> code = s).minWidth(0).grow().pad(0, 5, 0, 5);
+                else t.labelWrap(code).grow().pad(0, 5, 0, 5);
+                t.button(Icon.pencilSmall, Styles.emptyi, () -> {
+                    edit = !edit;
+                    codeCellBuild();
+                }).size(35).right();
+                t.button(Icon.addSmall, Styles.emptyi, () -> {
+                    codeCells.add(codeCells.indexOf(this) + 1, new CodeCell("+", "", true));
+                    rebuild();
+                }).size(35).right();
+                t.button(Icon.refreshSmall, Styles.emptyi, () -> {
+                    code = codeOrigin;
+                    codeCellBuild();
+                }).size(35).right();
+                t.button(Icon.cancelSmall, Styles.emptyi, () -> {
+                    codeCells.remove(this);
+                    rebuild();
+                }).size(35).right();
+            }).minHeight(35).growX();
+        }
+
+        private void rebuild() {
+            editPage.clear();
+            editPage.top();
+            editPage.add(editTools).growX();
+            editPage.row();
+            editPage.pane(p -> {
+                p.top();
+                for (CodeCell codeCell : codeCells) {
+                    p.add(codeCell).growX();
+                    p.row();
+                }
+            }).grow().update(p -> {
+                Element e = Core.scene.hit(Core.input.mouseX(), Core.input.mouseY(), true);
+                if (e != null && e.isDescendantOf(p)) p.requestScroll();
+                else if (p.hasScroll()) Core.scene.setScrollFocus(null);
+            }).with(p -> {
+                p.setupFadeScrollBars(0.5f, 0.25f);
+                p.setFadeScrollBars(true);
+                p.setScrollingDisabled(true, false);
+            });
         }
     }
 
     private final ArrayList<LExecutor.Var> constants;
     private final ArrayList<LExecutor.Var> links;
-    private final ArrayList<VarTable> varTables;
+    private final ArrayList<VarCell> varCells;
+    private final ArrayList<CodeCell> codeCells;
 
     public LogicMonitor(String text, LogicBlock.LogicBuild logicBuild, Vec2 pos) {
         super(text, logicBuild, pos);
@@ -53,7 +132,8 @@ public class LogicMonitor extends Monitor {
         editPage = new Table();
         constants = new ArrayList<>();
         links = new ArrayList<>();
-        varTables = new ArrayList<>();
+        varCells = new ArrayList<>();
+        codeCells = new ArrayList<>();
         varToolsBuild();
         varPageBuild();
         editToolsBuild();
@@ -112,15 +192,13 @@ public class LogicMonitor extends Monitor {
         varPage.row();
         constants.clear();
         links.clear();
-        varTables.clear();
+        varCells.clear();
         varPage.pane(p -> {
             p.top();
             for (LExecutor.Var var : logicBuild.executor.vars) {
                 if (!var.constant) {
                     if (checkVarName(var.name)) {
-                        VarTable varTable = new VarTable(var, var.name);
-                        varTables.add(varTable);
-                        p.add(varTable).growX();
+                        p.add(new VarCell(var, var.name)).growX();
                         p.row();
                     }
                 } else if (var.name.startsWith("@")) {
@@ -130,33 +208,29 @@ public class LogicMonitor extends Monitor {
                 }
             }
             for (LExecutor.Var var : constants) {
-                VarTable varTable = new VarTable(var, var.name);
-                varTables.add(varTable);
-                p.add(varTable).growX();
+                p.add(new VarCell(var, var.name)).growX();
                 p.row();
             }
             for (int i = 0; i < links.size(); i++) {
                 LExecutor.Var var = links.get(i);
-                VarTable varTable = new VarTable(var, "[" + i + "] " + var.name);
-                varTables.add(varTable);
-                p.add(varTable).growX();
+                p.add(new VarCell(var, "[" + i + "] " + var.name)).growX();
                 p.row();
             }
         }).grow().update(p -> {
             Element e = Core.scene.hit(Core.input.mouseX(), Core.input.mouseY(), true);
             if (e != null && e.isDescendantOf(p)) {
                 p.requestScroll();
-                for (VarTable varTable : varTables) {
-                    if (e.isDescendantOf(varTable))
-                        if (varTable.var.isobj && !(varTable.var.objval instanceof String))
-                            if (varTable.var.objval instanceof Unit unit) {
+                for (VarCell varCell : varCells) {
+                    if (e.isDescendantOf(varCell))
+                        if (varCell.var.isobj && !(varCell.var.objval instanceof String))
+                            if (varCell.var.objval instanceof Unit unit) {
                                 Drawf.select(unit.x, unit.y, unit.type.hitSize, Color.valueOf("00ffff"));
                                 Drawf.line(Color.valueOf("00ffff"), logicBuild.x, logicBuild.y, unit.x, unit.y);
-                                Fonts.outline.draw(varTable.var.name, unit.x, unit.y - unit.type.hitSize - 4, Color.valueOf("00ffff"), 0.4f, false, Align.center);
-                            } else if (varTable.var.objval instanceof Building building) {
+                                Fonts.outline.draw(varCell.var.name, unit.x, unit.y - unit.type.hitSize - 4, Color.valueOf("00ffff"), 0.4f, false, Align.center);
+                            } else if (varCell.var.objval instanceof Building building) {
                                 Drawf.select(building.x, building.y, building.block.size * 4, Color.valueOf("00ffff"));
                                 Drawf.line(Color.valueOf("00ffff"), logicBuild.x, logicBuild.y, building.x, building.y);
-                                Fonts.outline.draw(varTable.var.name, building.x, building.y - building.block.size * 4 - 4, Color.valueOf("00ffff"), 0.4f, false, Align.center);
+                                Fonts.outline.draw(varCell.var.name, building.x, building.y - building.block.size * 4 - 4, Color.valueOf("00ffff"), 0.4f, false, Align.center);
                             }
                 }
             } else if (p.hasScroll()) Core.scene.setScrollFocus(null);
@@ -168,6 +242,8 @@ public class LogicMonitor extends Monitor {
 
     private void editToolsBuild() {
         editTools.table(t -> {
+            t.button(Icon.refresh, Styles.emptyi, this::editPageBuild).grow();
+            t.button(Icon.upload, Styles.emptyi, this::uploadCode).grow();
             t.button(Icon.edit, Styles.emptyi, () -> {
                 if (showVarPage) showEditPage = false;
                 else showVarPage = true;
@@ -181,22 +257,30 @@ public class LogicMonitor extends Monitor {
         editPage.clear();
         editPage.top();
         editPage.add(editTools).growX();
-    }
-
-    private String formatVarText(LExecutor.Var var) {
-        if (var.isobj)
-            if (var.objval instanceof String) return '"' + var.objval.toString() + '"';
-            else if (var.objval == null) return "null";
-            else if (var.objval instanceof Unit unit) {
-                return '[' + unit.type.name + '#' + unit.id + "]\n[" + BigDecimal.valueOf(unit.flag).stripTrailingZeros().toPlainString() + "]";
-            } else if (var.objval instanceof Building building) {
-                return building.block.name + '#' + building.id;
-            } else return var.objval.toString();
-        else return BigDecimal.valueOf(var.numval).stripTrailingZeros().toPlainString();
+        editPage.row();
+        codeCells.clear();
+        editPage.pane(p -> {
+            p.top();
+            String[] codeList = logicBuild.code.split("\n");
+            for (int i = 0; i < codeList.length; i++) {
+                p.add(new CodeCell("#" + i, codeList[i], false)).growX();
+                p.row();
+            }
+        }).grow().update(p -> {
+            Element e = Core.scene.hit(Core.input.mouseX(), Core.input.mouseY(), true);
+            if (e != null && e.isDescendantOf(p)) p.requestScroll();
+            else if (p.hasScroll()) Core.scene.setScrollFocus(null);
+        }).with(p -> {
+            p.setupFadeScrollBars(0.5f, 0.25f);
+            p.setFadeScrollBars(true);
+            p.setScrollingDisabled(true, false);
+        });
     }
 
     private boolean checkVarName(String name) {
-        int index = getIndex(name);
+        int index;
+        if (filterCc) index = name.indexOf(varFilter);
+        else index = name.toUpperCase().indexOf(varFilter.toUpperCase());
         if (index < 0) return false;
         if (filterW) {
             boolean filter = true;
@@ -209,12 +293,10 @@ public class LogicMonitor extends Monitor {
         return true;
     }
 
-    private int getIndex(String name) {
-        if (filterCc) {
-            return name.indexOf(varFilter);
-        } else {
-            return name.toUpperCase().indexOf(varFilter.toUpperCase());
-        }
+    private void uploadCode() {
+        StringBuilder builder = new StringBuilder();
+        for (CodeCell codeCell : codeCells) builder.append(codeCell.code).append("\n");
+        logicBuild.updateCode(builder.toString());
     }
 
     @Override
