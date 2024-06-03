121c566cd48247201e4b8cd8df3c352edd81b9ae
yaddrx2 <yaddrx2@foxmail.com>
2024-06-03 13:08:14 +0800

    add memory binary int page

--- src/yr2lm/ui/MemoryMonitor.java
+++ src/yr2lm/ui/MemoryMonitor.java
@@ -11,46 +11,44 @@ import mindustry.ui.Styles;
 import mindustry.world.blocks.logic.MemoryBlock;
 
 import java.math.BigDecimal;
-import java.util.ArrayList;
 
 import static arc.scene.ui.TextField.TextFieldFilter.digitsOnly;
 
 public class MemoryMonitor extends Monitor {
     private final MemoryBlock.MemoryBuild memoryBuild;
     private int start = 0, end, step = 1, col = 8;
-    private boolean editMode = false, binMode = false;
+    private boolean editMode = false, intMode = false, binMode = false;
     private double[] memoryBuf;
-    private final Table memTools, nomPage, binPage;
+    private final Table memTools, nomPage, intPage, binPage;
 
     private class BinCell extends Table {
 
-        private final ArrayList<String> binText;
+        private final String[] binText;
         private int exponent = 0;
 
         public BinCell(int index) {
             super();
-            binText = new ArrayList<>();
-            for (int i = 0; i < 64; i++) binText.add("0");
-            setLabelProperty(label(() -> binText.get(0).equals("1") ? "■" : "□").grow().get(), 0, index);
+            binText = new String[64];
+            setLabelProperty(label(() -> binText[0]).grow().get(), 0, index);
             table().grow();
             for (int i = 1; i < 12; i++) {
                 int finalI = i;
-                setLabelProperty(label(() -> binText.get(finalI).equals("1") ? "■" : "□").grow().get(), finalI, index);
+                setLabelProperty(label(() -> binText[finalI]).grow().get(), finalI, index);
             }
             row();
             for (int i = 12; i < 64; i++) {
                 int finalI = i;
-                setLabelProperty(label(() -> (exponent == finalI ? "[#00ffff]" : "") + (binText.get(finalI).equals("1") ? "■" : "□")).grow().get(), finalI, index);
+                setLabelProperty(label(() -> (exponent == finalI ? "[#00ffff]" : "") + binText[finalI]).grow().get(), finalI, index);
                 if (i % 13 == 11) row();
             }
             double[] memoryInput = editMode ? memoryBuf : memoryBuild.memory;
             update(() -> {
                 String[] binaryStr = Long.toBinaryString(Double.doubleToLongBits(memoryInput[index])).split("");
                 int length = 64 - binaryStr.length;
-                for (int i = 0; i < length; i++) binText.set(i, "0");
-                for (int i = length; i < 64; i++) binText.set(i, binaryStr[i - length]);
+                for (int i = 0; i < length; i++) binText[i] = "□";
+                for (int i = length; i < 64; i++) binText[i] = binaryStr[i - length].equals("1") ? "■" : "□";
                 StringBuilder sb = new StringBuilder();
-                for (int i = 1; i < 12; i++) sb.append(binText.get(i));
+                for (int i = 1; i < 12; i++) sb.append(binText[i].equals("■") ? "1" : "0");
                 exponent = Integer.parseInt(sb.toString(), 2) - 1012;
             });
         }
@@ -58,8 +56,44 @@ public class MemoryMonitor extends Monitor {
         private void setLabelProperty(Label lb, int binPos, int bufPos) {
             lb.setAlignment(Align.center);
             if (editMode) lb.clicked(() -> {
-                binText.set(binPos, binText.get(binPos).equals("0") ? "1" : "0");
-                memoryBuf[bufPos] = Double.longBitsToDouble(Long.parseUnsignedLong(String.join("", binText), 2));
+                binText[binPos] = binText[binPos].equals("□") ? "■" : "□";
+                StringBuilder sb = new StringBuilder();
+                for (String s : binText) sb.append(s.equals("■") ? "1" : "0");
+                memoryBuf[bufPos] = Double.longBitsToDouble(Long.parseUnsignedLong(sb.toString(), 2));
+            });
+        }
+    }
+
+    private class IntCell extends Table {
+
+        private final String[] binText;
+        private boolean showText = false;
+
+        public IntCell(int index) {
+            super();
+            binText = new String[52];
+            for (int i = 0; i < 52; i++) {
+                int finalI = i;
+                setLabelProperty(label(() -> showText ? (finalI % 8 == 3 ? "[#00fff]" : "") + binText[finalI] : "").grow().get(), finalI, index);
+            }
+            double[] memoryInput = editMode ? memoryBuf : memoryBuild.memory;
+            update(() -> {
+                showText = memoryInput[index] >= 0 && memoryInput[index] % 1 == 0;
+                if (!showText) return;
+                String[] binaryStr = Integer.toString((int) memoryInput[index], 2).split("");
+                int length = 52 - binaryStr.length;
+                for (int i = 0; i < length; i++) binText[i] = "□";
+                for (int i = length; i < 52; i++) binText[i] = binaryStr[i - length].equals("1") ? "■" : "□";
+            });
+        }
+
+        private void setLabelProperty(Label lb, int binPos, int bufPos) {
+            lb.setAlignment(Align.center);
+            if (editMode) lb.clicked(() -> {
+                binText[binPos] = binText[binPos].equals("□") ? "■" : "□";
+                StringBuilder sb = new StringBuilder();
+                for (String s : binText) sb.append(s.equals("■") ? "1" : "0");
+                memoryBuf[bufPos] = Integer.parseInt(sb.toString(), 2);
             });
         }
     }
@@ -69,6 +103,7 @@ public class MemoryMonitor extends Monitor {
         this.memoryBuild = memoryBuild;
         memTools = new Table();
         nomPage = new Table();
+        intPage = new Table();
         binPage = new Table();
         end = ((MemoryBlock) this.memoryBuild.block).memoryCapacity;
         memToolsBuild();
@@ -78,6 +113,7 @@ public class MemoryMonitor extends Monitor {
     @Override
     public void init() {
         nomPageBuild();
+        intPageBuild();
         binPageBuild();
         monitorTableBuild();
     }
@@ -88,6 +124,7 @@ public class MemoryMonitor extends Monitor {
         monitorTable.add(memTools).growX();
         monitorTable.row();
         if (binMode) monitorTable.add(binPage).grow();
+        else if (intMode) monitorTable.add(intPage).grow();
         else monitorTable.add(nomPage).grow();
     }
 
@@ -121,9 +158,14 @@ public class MemoryMonitor extends Monitor {
                     col = 8;
                 }
             }).minWidth(0).grow().pad(0, 5, 0, 5);
-            t.button(Icon.refresh, Styles.emptyi, this::init).size(50);
-            t.button(Icon.grid, Styles.emptyi, () -> {
+            t.button(Icon.info, Styles.emptyi, () -> {
                 binMode = !binMode;
+                if (binMode) intMode = false;
+                monitorTableBuild();
+            }).size(50);
+            t.button(Icon.grid, Styles.emptyi, () -> {
+                intMode = !intMode;
+                if (intMode) binMode = false;
                 monitorTableBuild();
             }).size(50);
             t.button(Icon.edit, Styles.emptyi, () -> {
@@ -216,6 +258,27 @@ public class MemoryMonitor extends Monitor {
             p.setupFadeScrollBars(0.5f, 0.25f);
             p.setFadeScrollBars(true);
             p.setScrollingDisabled(true, false);
+        })).grow();
+    }
+
+    private void intPageBuild() {
+        intPage.clear();
+        intPage.table(t -> t.pane(p -> {
+            p.top();
+            int bound = end / step;
+            for (int i = start; i < bound; i++) {
+                int index = i * step;
+                p.add(new IntCell(index)).height(35).growX().pad(0, 5, 0, 10);
+                p.labelWrap((editMode ? "[#00ffff]#" : "#") + index).size(60, 35).top().pad(0, 10, 0, 5);
+                p.row();
+            }
+        }).grow().update(p -> {
+            Element e = Core.scene.hit(Core.input.mouseX(), Core.input.mouseY(), true);
+            if (e != null && e.isDescendantOf(p)) p.requestScroll();
+            else if (p.hasScroll()) Core.scene.setScrollFocus(null);
+        }).with(p -> {
+            p.setupFadeScrollBars(0.5f, 0.25f);
+            p.setFadeScrollBars(true);
         })).grow();
     }
 
